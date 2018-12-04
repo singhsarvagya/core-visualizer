4fe710d958d71f4a488fb080dd5d727193ba8466
Sarvagya Singh <sarvagya_singh@berkeley.edu>
2018-12-03 21:12:12 -0800

    Added install-deps.sh, run.sh and .gitignore

---
+++ install-deps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# installing python-3
+sudo apt-get install python3
+
+# installing tkinter for python 3
+sudo apt-get install python3-tk
+
+# installing matplotlib for python 3
+sudo apt-get install python3-matplotlib
